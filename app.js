01b2f17f702a63dcbc787770771e456960344591
Crazyartist5 <97100406+AnjaliPrashant@users.noreply.github.com>
2022-04-09 19:08:23 +0530

    website

---
+++ app.js
@@ -0,0 +1,80 @@
+//Animals
+const catsound = new Audio("catsound.mp3");
+document.getElementById("animal1").addEventListener
+    ("click", function () {
+        catsound.play();
+    });
+const dogsound = new Audio("dogsound.mp3");
+document.getElementById("animal2").addEventListener
+    ("click", function () {
+        dogsound.play();
+    });
+const elephantsound = new Audio("elephantsound.mp3");
+document.getElementById("animal3").addEventListener
+    ("click", function () {
+        elephantsound.play();
+    });
+const gorrilasound = new Audio("gorrilasound.wav");
+document.getElementById("animal4").addEventListener
+    ("click", function () {
+        gorrilasound.play();
+    });
+const lionsound = new Audio("lionsound.mp3");
+document.getElementById("animal5").addEventListener
+    ("click", function () {
+        lionsound.play();
+    });
+//Birds
+const crowsound = new Audio("crowsound.mp3");
+document.getElementById("bird1").addEventListener
+    ("click", function () {
+        crowsound.play();
+    });
+const eaglesound = new Audio("eaglesound.mp3");
+document.getElementById("bird2").addEventListener
+    ("click", function () {
+        eaglesound.play();
+    });
+const parrotsound = new Audio("parrotsound.mp3");
+document.getElementById("bird3").addEventListener
+    ("click", function () {
+        parrotsound.play();
+    });
+const peacocksound = new Audio("peacocksound.mp3");
+document.getElementById("bird4").addEventListener
+    ("click", function () {
+        peacocksound.play();
+    });
+const hensound = new Audio("hensound.mp3");
+document.getElementById("bird5").addEventListener
+    ("click", function () {
+        hensound.play();
+    });
+//Humans
+const crysound = new Audio("crysound.mp3");
+document.getElementById("human1").addEventListener
+    ("click", function () {
+        crysound.play();
+    });
+const laughsound = new Audio("laughsound.mp3");
+document.getElementById("human2").addEventListener
+    ("click", function () {
+        laughsound.play();
+    });
+const sneezingsound = new Audio("sneezingsound.mp3");
+document.getElementById("human3").addEventListener
+    ("click", function () {
+        sneezingsound.play();
+    });
+const coughsound = new Audio("coughsound.mp3");
+document.getElementById("human4").addEventListener
+    ("click", function () {
+        coughsound.play();
+    });
+const hurraysound = new Audio("hurraysound.mp3");
+document.getElementById("human5").addEventListener
+    ("click", function () {
+        hurraysound.play();
+    });
+
+
